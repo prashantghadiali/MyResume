24ca6d00fe88197964c4028ba88ca0b2d18516e6
Prashant Ghadiali <er.prashantghadiali@gmail.com>
2023-08-05 13:57:03 +0530

    request handler

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const http = require('http');
 const port = 8000;     // port is defined
 
-const server = http.createServer();    // server is created
+function requestHandler(req, res){
+    console.log(req.url)    // it prints url in console when this fun is passed in const server function
+
+    // res.end("Get it!")   // this string is printed on response side or on Frontend side
+}
+
+const server = http.createServer(requestHandler);    // server is created and passed request handler to hit that function while creating server
 
 server.listen(port,function(err){
     if (err) {
